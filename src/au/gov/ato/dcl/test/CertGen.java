b0f425344cb9291838cf946d4f7cad4a73bc3417
Martijn van den Boogaard <martijn.vandenboogaard@gmail.com>
2018-12-13 15:36:16 +1100

    Adding key usage and extended usage attributes Change signing algorithm Fixed signing issues

--- src/au/gov/ato/dcl/test/CertGen.java
+++ src/au/gov/ato/dcl/test/CertGen.java
@@ -5,7 +5,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringWriter;
 import java.math.BigInteger;
 import java.security.GeneralSecurityException;
 import java.security.KeyFactory;
@@ -15,7 +14,6 @@ import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
-import java.security.Signature;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateEncodingException;
 import java.security.cert.CertificateException;
@@ -23,37 +21,52 @@ import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Base64;
 import java.util.Date;
-
-import javax.xml.bind.DatatypeConverter;
+import java.util.Vector;
 
 import sun.misc.BASE64Decoder;
+import sun.security.util.ObjectIdentifier;
 import sun.security.x509.AlgorithmId;
+import sun.security.x509.AuthorityKeyIdentifierExtension;
 import sun.security.x509.CertificateAlgorithmId;
+import sun.security.x509.CertificateExtensions;
 import sun.security.x509.CertificateIssuerName;
 import sun.security.x509.CertificateSerialNumber;
 import sun.security.x509.CertificateValidity;
 import sun.security.x509.CertificateVersion;
 import sun.security.x509.CertificateX509Key;
+import sun.security.x509.ExtendedKeyUsageExtension;
+import sun.security.x509.KeyIdentifier;
+import sun.security.x509.KeyUsageExtension;
+import sun.security.x509.SubjectKeyIdentifierExtension;
 import sun.security.x509.X500Name;
 import sun.security.x509.X509CertImpl;
 import sun.security.x509.X509CertInfo;
 
 public class CertGen {
 
-	public static String certChain(String dn, String privKeyPath) throws GeneralSecurityException, IOException {
-		return CertGen.caCertPEM().concat(CertGen.certToString(CertGen.apply(dn, privKeyPath)));
+	public static String base64CertChain(String dn, String privKeyPath, String pubKeyPath) throws GeneralSecurityException, IOException {
+		final String LINE_SEPARATOR = System.getProperty("line.separator");
+//		final Base64.Encoder encoder = Base64.getMimeEncoder(64, LINE_SEPARATOR.getBytes());
+		final Base64.Encoder encoder = Base64.getEncoder();
+		byte[] encoded = encoder.encode(CertGen.certChain(dn, privKeyPath, pubKeyPath).getBytes());
+		return new String(encoded);
+	}
+	
+	public static String certChain(String dn, String privKeyPath, String pubKeyPath) throws GeneralSecurityException, IOException {
+		return String.join("", CertGen.caCertPEM(pubKeyPath), CertGen.certToString(CertGen.apply(dn, privKeyPath, pubKeyPath)));
 	}
 
-	public static X509Certificate apply(String dn, String privKeyPath) throws GeneralSecurityException, IOException {
+	public static X509Certificate apply(String dn, String privKeyPath, String pubKeyPath) throws GeneralSecurityException, IOException {
 		final KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-		keyGen.initialize(1024);
+		keyGen.initialize(2048);
 		final KeyPair keypair = keyGen.generateKeyPair();
 		// PrivateKey privKey = keypair.getPrivate();
 		// PublicKey pubKey = keypair.getPublic();
 		final PrivateKey caKey = CertGen.getCaKeyFromString(CertGen.caKeyPEM(privKeyPath));
-		final Certificate caCert = CertGen.getCACertFromString(CertGen.caCertPEM());
-		return generateCertificate(dn, keypair, 365, "SHA1withRSA", caKey, caCert);
+		final Certificate caCert = CertGen.getCACertFromString(CertGen.caCertPEM(pubKeyPath));
+		return generateCertificate(dn, keypair, 365, "SHA256withRSA", caKey, caCert);
 	}
 
 	/**
@@ -75,10 +88,10 @@ public class CertGen {
 	 * Read the CA public certificate from bundle
 	 * 
 	 * @return base64 encoded CA public key
+	 * @throws FileNotFoundException 
 	 */
-	public static String caCertPEM() {
-		final InputStream in = new CertGen().getClass()
-				.getResourceAsStream("/resources/Digital_Capability_Locator_Authority.crt");
+	public static String caCertPEM(String pubKeyPath) throws FileNotFoundException {
+		final InputStream in = new FileInputStream(pubKeyPath);
 		final java.util.Scanner scanner = new java.util.Scanner(in);
 		final java.util.Scanner s = scanner.useDelimiter("\\A");
 		final String certPEM = s.hasNext() ? s.next() : "";
@@ -149,15 +162,7 @@ public class CertGen {
 		final Date to = new Date(from.getTime() + days * 86400000l);
 		final CertificateValidity interval = new CertificateValidity(from, to);
 		final BigInteger sn = new BigInteger(64, new SecureRandom());
-		final X500Name owner = new X500Name(dn);
-
-		Signature signature;
-		try {
-			signature = Signature.getInstance(algorithm);
-		} catch (NoSuchAlgorithmException e) {
-			throw new RuntimeException(e);
-		}
-		signature.initSign(caKey);
+		final X500Name subject = new X500Name(dn);
 
 		final byte[] encoded = caCert.getEncoded();
 		final X509CertImpl caCertImpl = new X509CertImpl(encoded);
@@ -165,15 +170,54 @@ public class CertGen {
 		final X509CertInfo caCertInfo = (X509CertInfo) caCertImpl.get(X509CertImpl.NAME + "." + X509CertImpl.INFO);
 
 		final X500Name issuer = (X500Name) caCertInfo.get(X509CertInfo.SUBJECT + "." + CertificateIssuerName.DN_NAME);
+		CertificateExtensions exts = new CertificateExtensions();
+		
+		// Extensions[6]: X509v3 Key Usage
+        KeyUsageExtension keyUsage = new KeyUsageExtension();
+        keyUsage.set(KeyUsageExtension.DIGITAL_SIGNATURE, Boolean.TRUE);
+        keyUsage.set(KeyUsageExtension.NON_REPUDIATION, Boolean.FALSE);
+        keyUsage.set(KeyUsageExtension.KEY_ENCIPHERMENT, Boolean.TRUE);
+        keyUsage.set(KeyUsageExtension.DATA_ENCIPHERMENT, Boolean.TRUE);
+        keyUsage.set(KeyUsageExtension.KEY_AGREEMENT, Boolean.TRUE);
+        keyUsage.set(KeyUsageExtension.KEY_CERTSIGN, Boolean.FALSE);
+        keyUsage.set(KeyUsageExtension.CRL_SIGN, Boolean.FALSE);
+        keyUsage.set(KeyUsageExtension.ENCIPHER_ONLY, Boolean.FALSE);
+        keyUsage.set(KeyUsageExtension.DECIPHER_ONLY, Boolean.FALSE);
+        exts.set(KeyUsageExtension.NAME, keyUsage);
+        
+	     // Extensions[5]: X509v3 Extended Key Usage
+		Vector<ObjectIdentifier> keyUsages = new Vector<>();
+       	// OID defined in RFC 3280 Sections 4.2.1.13
+       	// more from http://www.alvestrand.no/objectid/1.3.6.1.5.5.7.3.html
+       	// serverAuth
+       	keyUsages.addElement(ObjectIdentifier.newInternal(new int[] { 1, 3, 6, 1, 5, 5, 7, 3, 1 }));
+       	// clientAuth
+       	keyUsages.addElement(ObjectIdentifier.newInternal(new int[] { 1, 3, 6, 1, 5, 5, 7, 3, 2 }));
+       	exts.set(ExtendedKeyUsageExtension.NAME, new ExtendedKeyUsageExtension(keyUsages));
+ 
+		  // Extensions[8]: X509v3 Subject Key Identifier
+        exts.set(SubjectKeyIdentifierExtension.NAME,
+                new SubjectKeyIdentifierExtension(new KeyIdentifier(pair.getPublic()).getIdentifier()));
 
+        // Extensions[2]: X509v3 Authority Key Identifier
+        exts.set(AuthorityKeyIdentifierExtension.NAME,
+                new AuthorityKeyIdentifierExtension(new KeyIdentifier(caCert.getPublicKey()),
+                        null, null));
+
+//        exts.set(AuthorityKeyIdentifierExtension.NAME,
+//                new AuthorityKeyIdentifierExtension(new KeyIdentifier(caCert.getPublicKey()),
+//                        new GeneralNames().add(new GeneralName(subject)), new SerialNumber(1)));
+ 
 		info.set(X509CertInfo.VALIDITY, interval);
 		info.set(X509CertInfo.SERIAL_NUMBER, new CertificateSerialNumber(sn));
-		info.set(X509CertInfo.SUBJECT, owner);
+		info.set(X509CertInfo.SUBJECT, subject);
 		info.set(X509CertInfo.ISSUER, issuer);
 		info.set(X509CertInfo.KEY, new CertificateX509Key(pair.getPublic()));
 		info.set(X509CertInfo.VERSION, new CertificateVersion(CertificateVersion.V3));
-		AlgorithmId algo = new AlgorithmId(AlgorithmId.sha1WithRSAEncryption_oid);
+		AlgorithmId algo = new AlgorithmId(AlgorithmId.sha256WithRSAEncryption_oid);
 		info.set(X509CertInfo.ALGORITHM_ID, new CertificateAlgorithmId(algo));
+
+		info.set(X509CertInfo.EXTENSIONS, exts);
 
 		// Sign the cert to identify the algorithm that's used.
 		X509CertImpl cert = new X509CertImpl(info);
@@ -183,19 +227,16 @@ public class CertGen {
 		algo = (AlgorithmId) cert.get(X509CertImpl.SIG_ALG);
 		info.set(CertificateAlgorithmId.NAME + "." + CertificateAlgorithmId.ALGORITHM, algo);
 		cert = new X509CertImpl(info);
-		cert.sign(privkey, algorithm);
+		cert.sign(caKey, algorithm);
 		return cert;
 	}
 
-	private static String certToString(X509Certificate cert) {
-		final StringWriter sw = new StringWriter();
-		try {
-			sw.write("-----BEGIN CERTIFICATE-----\n");
-			sw.write(DatatypeConverter.printBase64Binary(cert.getEncoded()).replaceAll("(.{64})", "$1\n"));
-			sw.write("\n-----END CERTIFICATE-----\n");
-		} catch (CertificateEncodingException e) {
-			e.printStackTrace();
-		}
-		return sw.toString();
+	public static String certToString(X509Certificate cert) throws CertificateEncodingException {
+		final String BEGIN_CERT = "-----BEGIN CERTIFICATE-----";
+		final String END_CERT = "-----END CERTIFICATE-----";
+		final String LINE_SEPARATOR = System.getProperty("line.separator");
+		final Base64.Encoder encoder = Base64.getMimeEncoder(64, LINE_SEPARATOR.getBytes());
+	    final String encodedCertText = new String(encoder.encode(cert.getEncoded()));
+	    return String.join(LINE_SEPARATOR, BEGIN_CERT, encodedCertText, END_CERT);
 	}
 }
--- test/TestCertGen.java
+++ test/TestCertGen.java
@@ -16,7 +16,9 @@ class TestCertGen {
 			String path = props.getProperty("user.dir");
 			String fs = props.getProperty("file.separator");
 			String keyFile = String.join(fs, path, "Digital_Capability_Locator_Authority.key");
-			X509Certificate cert = au.gov.ato.dcl.test.CertGen.apply("CN=test", keyFile);
+			String certFile = String.join(fs,  path, "Digital_Capability_Locator_Authority.crt");
+			X509Certificate cert = au.gov.ato.dcl.test.CertGen.apply("CN=test", keyFile, certFile);
+			System.err.println(cert);
 			assertEquals("Subject DN equals", "CN=test", cert.getSubjectDN().getName());		
 		} catch (GeneralSecurityException e) {
 			// TODO Auto-generated catch block
@@ -34,7 +36,9 @@ class TestCertGen {
 			String path = props.getProperty("user.dir");
 			String fs = props.getProperty("file.separator");
 			String keyFile = String.join(fs, path, "Digital_Capability_Locator_Authority.key");
-			String chain = au.gov.ato.dcl.test.CertGen.certChain("CN=test", keyFile);
+			String certFile = String.join(fs,  path, "Digital_Capability_Locator_Authority.crt");
+
+			String chain = au.gov.ato.dcl.test.CertGen.base64CertChain("CN=test", keyFile, certFile);
 			System.err.println(chain);
 		} catch (GeneralSecurityException e) {
 			// TODO Auto-generated catch block
@@ -45,4 +49,23 @@ class TestCertGen {
 		}
 	}
 
+	@Test
+	void test3() {
+		try {
+			Properties props = System.getProperties();
+			String path = props.getProperty("user.dir");
+			String fs = props.getProperty("file.separator");
+			String keyFile = String.join(fs, path, "Digital_Capability_Locator_Authority.key");
+			String certFile = String.join(fs,  path, "Digital_Capability_Locator_Authority.crt");
+
+			String chain = au.gov.ato.dcl.test.CertGen.certChain("CN=test", keyFile, certFile);
+			System.err.println(chain);
+		} catch (GeneralSecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
